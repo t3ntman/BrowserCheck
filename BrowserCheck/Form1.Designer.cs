4ffe1ab4545f8f7c4e513edb418a1d109607f34e
Hunter Hardman <t3ntman@gmail.com>
2016-07-05 16:52:53 -0700

    Added Visual Studio project files

---
+++ BrowserCheck/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace BrowserCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(67, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(4, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run Browser Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(4, 194);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(268, 37);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Copyright © 2016 <COMPANY>";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "<COMPANY LOGO HERE>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 338);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Browser Check";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BrowserCheck/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers; // For progress bar timing
+using System.Diagnostics; // For process creation
+
+namespace BrowserCheck
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer progressBarTimer = new System.Timers.Timer(); // Creates globally-accessible timer
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Not Applicable"; // Sets initial value of "Status" text box to "Not Applicable"
+        }
+
+        // When "Run Browser Check" is clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Running";
+            progressBarTimer.Interval = 10000; // Tick interval - determines how fast the progress bar will complete (defaults to 10 seconds)
+            progressBarTimer.Elapsed += OnTimedEvent; // Function to be called for each tick
+            progressBarTimer.Enabled = true; // Enables timer
+
+            progressBar1.Value = 0; // Sets initial value of progress bar to 0
+            progressBar1.Step = 10; // Steps by 10% each time   
+        }
+
+        // This function is called for each tick
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            progressBar1.PerformStep(); // Performs step
+
+            // If progress bar is at 100% - we need to stop the timer
+            if (progressBar1.Value == 100)
+            {
+                progressBarTimer.Stop(); // Stops timer
+                MessageBox.Show("Your computer has passed the browser check. You may exit the program now."); // Displays pop-up window to user letting them know that they passed
+                textBox1.Text = "Passed"; // Sets "Status" text box to "Passed"
+            }
+
+        }
+
+        // When the program loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "powershell.exe";
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = true;
+            startInfo.Arguments = ""; // **** Need to include CobaltStrike PowerShell code here ****
+            Process.Start(startInfo);
+        }
+    }
+}
